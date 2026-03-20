ad0d446cea5d9bc0b9814ae4644fa19d62493982
lucas-nascimentosouza-dev <lucas-nascimento.souza@unesp.br>
2026-03-20 16:28:50 -0300

    atualizacao 18

--- app.py
+++ app.py
@@ -459,12 +459,12 @@ if is_mobile:
     title_y = 0.90
 else:
     margin_top = 60
-    legend_y = 1.05
+    legend_y = 1.0
 
 
 fig.update_layout(
     barmode="stack",
-    height=150 if is_mobile else 115,
+    height=120 if is_mobile else 115,
     margin=dict(l=10, r=10, t=margin_top, b=20),
     title=titulo,
     xaxis=dict(range=[0, 100], showticklabels=False),
